f295dad651ecd4a146d47c257edcc2bad4ff79e4
Max Edell <maxed@adobe.com>
2022-08-18 11:39:46 -0700

    fix: differentiate password from api key

--- worker/routes/auth.ts
+++ worker/routes/auth.ts
@@ -10,13 +10,80 @@
  * governing permissions and limitations under the License.
  */
 
-import type { Route } from '../types';
+import type { Route, Context } from '../types';
 
+export function isAuthenticated(request: Request, ctx: Context): boolean {
+  const { env } = ctx;
+
+  const token = request.headers.get('auth_token');
+  if (token && token === env.UI_PASSWORD) {
+    return true;
+  }
+
+  const cookieStr = request.headers.get('cookie');
+  if (!cookieStr) {
+    return false;
+  }
+
+  const cookies = cookieStr.split(';').map((s) => s.trim());
+
+  const obj: Record<string, string> = {};
+  cookies.forEach((c) => {
+    const [key, val] = c.split('=');
+    obj[key] = val;
+  });
+
+  if (!obj.auth_token || obj.auth_token !== env.UI_PASSWORD) {
+    return false;
+  }
+  return true;
+}
+
+export function unauthenticatedTemplate() {
+  return /* html */`<!DOCTYPE html>
+<html>
+  <head>
+    <script>
+      function getToken(){
+        return prompt('Please enter your password:', '');
+      }
+      async function auth(){
+        let token;
+        let retry = true;
+        while(!token && retry) {
+          token = getToken();
+          if(!token) {
+            retry = window.confirm('Invalid password, try again?');
+          }
+        }
+
+        if(token) {
+          await fetch('/auth', { 
+            method: 'POST', 
+            body: JSON.stringify({ token }) 
+          });
+          window.navigation.reload();
+        }
+      }
+    </script>
+  </head>
+  <body onload="auth()">
+</html>`;
+}
+
+export function unauthenticatedResponse() {
+  return new Response(unauthenticatedTemplate(), {
+    status: 200,
+    headers: {
+      'content-type': 'text/html; charset=utf-8',
+    },
+  });
+}
 const Auth: Route = async (request, ctx) => {
   const { env, log } = ctx;
   const json: Record<string, string> = await request.json();
   const { token } = json;
-  if (!token || token.trim() !== env.API_KEY) {
+  if (!token || token.trim() !== env.UI_PASSWORD) {
     log.warn('Not authorized: ', token);
     return new Response('Not authorized', { status: 401 });
   }
