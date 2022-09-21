a1572487acf714bdefb364418d5d72339b1e474c
Max Edell <maxed@adobe.com>
2022-09-21 12:33:53 -0700

    fix: related articles styling

--- blocks/article/article.js
+++ blocks/article/article.js
@@ -1,7 +1,11 @@
+// function templateRelatedTag(tag) {
+//   return `<li><a href="/${toClassName(tag)}/">${tag}</a></li>`;
+// }
+
 /**
  * @param {HTMLDivElement} block
  */
 // eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
-
+  console.log('block: ', block);
 }
